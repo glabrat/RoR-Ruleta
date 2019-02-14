443d166c9f8765f22e1309b09a4944c258a24a23
glabrat <gaj.labra@gmail.com>
2019-02-13 22:08:49 -0300

    created game room

---
+++ db/migrate/20190214010414_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.integer :min_players
+      t.integer :max_players
+      t.integer :time_rounds
+
+      t.timestamps
+    end
+  end
+end
